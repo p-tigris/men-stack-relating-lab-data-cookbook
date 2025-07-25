14e38ca745013322cb1e5dded15a3c73bad798c8
smdhar <shoumik_dhar@yahoo.com>
2025-07-25 12:58:38 -0400

    Added navbar, index and new pages

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
+router.get('/', (req, res) => {
+    res.render('foods/index.ejs');
+});
 
-
+router.get('/new', (req, res) => {
+    res.render('foods/new.ejs');
+} )
 
 
 
--- server.js
+++ server.js
@@ -38,14 +38,6 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
-});
-
 app.use(passUserToView);
 app.use('/auth', authController);
 app.use(isSignedIn);
